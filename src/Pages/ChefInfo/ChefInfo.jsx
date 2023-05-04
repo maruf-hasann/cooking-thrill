7ce767184fd9313c73e9c12c7fc02a22f9cd4244
afnafmafeee <afnafmafeee@gmail.com>
2023-05-04 14:56:36 +0600

    chefinfo page

--- src/Pages/ChefInfo/ChefInfo.jsx
+++ src/Pages/ChefInfo/ChefInfo.jsx
@@ -3,12 +3,21 @@ import { useLoaderData } from 'react-router-dom';
 
 const ChefInfo = () => {
     const data = useLoaderData()
-   const {name} = data
+   const { name, banner, img, bio, experience } = data;
     
     return (
-        <div>
-            <p>{name}</p>
+      <div className="my_container">
+        <div className="mb-7">
+          <img src={banner} alt="" className="rounded-2xl h-32 w-full " />
         </div>
+        <div className="">
+          <img src={img} alt="" className="w-1/4 rounded-lg" />
+          <p>{bio}</p>
+          <p className="font-semibold text-gray-800 text-xl">
+            {experience} Year Of experience
+          </p>
+        </div>
+      </div>
     );
 };
 
