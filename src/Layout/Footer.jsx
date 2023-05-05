e9cc8215557898d7345ad4b8224bf1c1b30ca88d
afnafmafeee <afnafmafeee@gmail.com>
2023-05-05 19:51:20 +0600

    blog area done

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,34 +1,53 @@
 import React from 'react';
+import F from '../assets/icons8-facebook-144.png'
+import I from '../assets/icons8-instagram-144.png'
+import T from '../assets/icons8-twitter-144.png'
+import Y from '../assets/icons8-youtube-144.png'
 
 const Footer = () => {
     return (
-      <div className="my_container">
-        <div className="flex justify-between">
-          <div className="w-2/4">
-            <h2 className=" mb-3 font-bold text-2xl text-gray-600">
-              Cooking thrill
-            </h2>
-            <p className="text-xl">
-              Create a website that shows the recipes of all available chefs.
-              The website must be exclusively dedicated to one
-              cuisine Bangladeshi, Chinese, Indian, Mediterranean, Thai,
-              Japanese, Italian, African, Vietnamese, Srilankan, Nepalese,
-              American, Korean, Mexican, Australian, Afgan, French, etc.
-            </p>
-          </div>
-          <div>
-            <h2 className=" mb-3 font-bold text-2xl text-gray-600">
-              Need Help?
-            </h2>
-            <p className="text-xl font-semibold text-gray-600">
-              Email:cook.thrill@yahoo.com
-            </p>
-            <p className="text-xl font-semibold text-gray-600">
-              Phone: 3262562234
-            </p>
-          </div>
-          <div>
-            <h2 className="font-bold text-2xl text-gray-600">Follow Us</h2>
+      <div className="bg-gray-50 ">
+        <div className="my_container" id="footer">
+          <div className="lg:flex lg:justify-between space-y-3 lg:space-y-0 gap-4">
+            <div className="lg:w-2/4">
+              <h2 className=" mb-3 font-bold text-2xl text-gray-600">
+                Cooking thrill
+              </h2>
+              <p className="text-xl">
+                Since our modest beginnings in 2020 with a little space in
+                Toronto’s stylish Yorkville locale, ‘Cooking Thrill’ ‘s
+                development has been enlivened with the energy to cook and serve
+                solid, Bangladeshi takeout food.
+              </p>
+            </div>
+            <div>
+              <h2 className=" mb-3 font-bold text-2xl text-gray-600">
+                Need Help?
+              </h2>
+              <p className="text-xl font-semibold text-gray-600">
+                Email:cook.thrill@yahoo.com
+              </p>
+              <p className="text-xl font-semibold text-gray-600">
+                Phone: 3262562234
+              </p>
+            </div>
+            <div>
+              <h2 className=" mb-3 font-bold text-2xl text-gray-600">
+                Location
+              </h2>
+              <p className="text-xl font-semibold text-gray-600">
+               Gazipur, Dhaka Bangladesh
+              </p>
+            </div>
+            <div>
+              <h2 className="font-bold text-2xl text-gray-600">Follow Us</h2>
+              <div className="lg:flex gap-4 mt-4">
+                <img src={F} alt="" className="w-11 h-11" />
+                <img src={I} alt="" className="w-11 h-11" />
+                <img src={T} alt="" className="w-11 h-11" />
+                <img src={Y} alt="" className="w-11 h-11" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -1,11 +1,61 @@
-import React from 'react';
+import React from "react";
 
 const Blogs = () => {
-    return (
+  return (
+    <div className="my_container">
+      <h1 className=" text-center text-4xl font-bold mb-14">Blogs</h1>
+      <div className="space-y-4">
         <div>
-          <h1>blogs website</h1>
+          <h2 className="font-semibold text-3xl mb-4">
+            Differences between uncontrolled and controlled components.
+          </h2>
+          <p>
+            In React, controlled components refer to components that have their
+            state and behavior controlled by the parent component. These
+            components rely on props passed down from the parent component to
+            update their state and behavior. Uncontrolled components refer to
+            components that manage their own state internally
+          </p>
+        </div>{" "}
+        <div>
+          <h2 className="font-semibold text-3xl mb-4">
+            Differences between uncontrolled and controlled components.
+          </h2>
+          <p>
+            React PropTypes validators : 1 any : The prop can be of any data
+            type. 2 bool : The prop should be a Boolean. 3 number : The prop
+            should be a number. 4 string : The prop should be a string. func :
+            The prop should be a function. 5 array : The prop should be an
+            array. 6 object: The prop should be an object
+          </p>
         </div>
-    );
+        <div>
+          <h2 className="font-semibold text-3xl mb-4">
+            Difference between nodejs and express js.
+          </h2>
+          <p>
+            NodeJS is an event-driven, non-blocking I/O model using JavaScript
+            as its main language. It helps to build scalable network
+            applications. Express is a minimal and flexible Node. js web
+            application framework that provides a robust set of features for web
+            and mobile applications.
+          </p>
+        </div>
+        <div>
+          <h2 className="font-semibold text-3xl mb-4">
+            What is a custom hook, and why will you create a custom hook?
+          </h2>
+          <p>
+            Custom React JS hooks offer reusability as when a custom hook is
+            created, it can be reused easily, which makes the code cleaner and
+            reduces the time to write the code. It also enhances the rendering
+            speed of the code as a custom hook does not need to be rendered
+            again and again while rendering the whole code.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
